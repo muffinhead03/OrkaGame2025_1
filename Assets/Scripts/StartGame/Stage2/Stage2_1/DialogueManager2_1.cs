55d278f61efec642d20be89c02f535499fa846d9
muffinhead03 <ilovedc10@naver.com>
2025-06-04 19:56:27 +0900

    2025년 6월 4일차

---
+++ Assets/Scripts/StartGame/Stage2/Stage2_1/DialogueManager2_1.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+
+public class DialogueManager2_1 : MonoBehaviour
+{
+    [Header("Components")]
+    public LocalizeStringEvent localizedStringEvent;
+    public Typewriter typeWriter;
+    public TMP_Text targetText;
+    public Button nextButton;
+
+    [Header("Sound Effects")]
+    public AudioSource birdAudio;
+    public AudioSource waterAudio;
+
+    private int index = 0;
+    private string tableName = "Stage2_1";
+    private string[] keySuffixes = {
+        "key2_1_1",
+        "key2_1_2",
+        "key2_1_3",
+        "key2_1_4"
+    };
+
+    private void Start()
+    {
+        nextButton.interactable = false;
+        nextButton.onClick.AddListener(NextDialogue);
+        StartCoroutine(PlayIntroSequence());
+    }
+
+    private IEnumerator PlayIntroSequence()
+    {
+        if (birdAudio) birdAudio.Play();
+        yield return new WaitForSeconds(0.5f);
+
+        if (waterAudio) waterAudio.Play();
+        yield return new WaitForSeconds(0.5f);
+
+        ShowDialogue(index);
+    }
+
+    private void ShowDialogue(int i)
+    {
+        if (i >= keySuffixes.Length)
+        {
+            SceneManager.LoadScene("Stage2_1");
+            return;
+        }
+
+        nextButton.interactable = false;
+
+        string key = keySuffixes[i];
+        localizedStringEvent.StringReference.SetReference(tableName, key);
+        localizedStringEvent.OnUpdateString.RemoveAllListeners();
+        localizedStringEvent.OnUpdateString.AddListener(OnLocalizedStringReady);
+        localizedStringEvent.RefreshString(); // 강제로 갱신
+    }
+
+    private void OnLocalizedStringReady(string localizedText)
+    {
+        StartCoroutine(StartTypingCoroutine(localizedText));
+    }
+
+    private IEnumerator StartTypingCoroutine(string fullText)
+    {
+        yield return StartCoroutine(typeWriter.Type(fullText));
+        nextButton.interactable = true;
+    }
+
+    private void NextDialogue()
+    {
+        index++;
+        ShowDialogue(index);
+    }
+}
