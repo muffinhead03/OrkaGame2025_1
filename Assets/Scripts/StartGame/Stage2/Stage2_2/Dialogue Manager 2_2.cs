a893feaa00f31b44be859ab7ff47e994604dd894
정희진 <mint02@ewhain.net>
2025-06-24 14:49:44 +0900

    스테이지2다시2 수정 완

--- Assets/Scripts/StartGame/Stage2/Stage2_2/Dialogue Manager 2_2.cs
+++ Assets/Scripts/StartGame/Stage2/Stage2_2/Dialogue Manager 2_2.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 using UnityEngine.Localization.Components;
-using UnityEngine.Localization;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 using System.Collections;
 
 public class DialogueManager2_2 : MonoBehaviour
 {
     [Header("UI Components")]
-    public LocalizeStringEvent aboveLineStringEvent;   // Stage2_2Above용
-    public LocalizeStringEvent storyLineStringEvent;   // Stage2_2용
+    public LocalizeStringEvent aboveLineStringEvent;
+    public LocalizeStringEvent storyLineStringEvent;
     public TMP_Text aboveLineText;
     public TMP_Text storyLineText;
     public Button nextButton;
@@ -20,15 +19,14 @@ public class DialogueManager2_2 : MonoBehaviour
     public GameObject echoImage;
 
     private int index = 0;
-    private string storyTableName = "Stage2_2";
-    private string aboveTableName = "Stage2_2Above";
+    private string storyTableName = "Stage2_2SL";
+    private string aboveTableName = "Stage2_2AL";
 
     private string[] storyKeys = {
         "key2_2_1", "key2_2_2", "key2_2_3", "key2_2_4", "key2_2_5",
         "key2_2_6", "key2_2_7", "key2_2_8", "key2_2_9", "key2_2_10"
     };
 
-    // 위 대사의 화자가 나르케인 경우
     private int[] narkeIndexes = { 0, 2, 4, 6, 7, 9 };
 
     private void Start()
@@ -49,44 +47,37 @@ public class DialogueManager2_2 : MonoBehaviour
         nextButton.gameObject.SetActive(false);
 
         bool isNarke = System.Array.Exists(narkeIndexes, n => n == i);
+        string aboveKey = (i % 2 == 0) ? "key1" : "key2";
 
-        // 위 대사 설정
-        aboveLineStringEvent.StringReference.SetReference(
-            aboveTableName,
-            isNarke ? "key2_2_above_narke" : "key2_2_above_echo"
-        );
-
-        // 스토리 대사 설정
-        storyLineStringEvent.StringReference.SetReference(
-            storyTableName,
-            storyKeys[i]
-        );
-
-        // 캐릭터 이미지 표시
-        narkeImage.SetActive(isNarke);
-        echoImage.SetActive(!isNarke);
-
-        // 위 대사 바인딩
+        aboveLineStringEvent.StringReference.SetReference(aboveTableName, aboveKey);
         aboveLineStringEvent.OnUpdateString.RemoveAllListeners();
         aboveLineStringEvent.OnUpdateString.AddListener(text => aboveLineText.text = text);
+        aboveLineStringEvent.RefreshString();
 
-        // 스토리 대사 바인딩
+        storyLineStringEvent.StringReference.SetReference(storyTableName, storyKeys[i]);
         storyLineStringEvent.OnUpdateString.RemoveAllListeners();
         storyLineStringEvent.OnUpdateString.AddListener(OnStoryLineReady);
-
-        // 텍스트 갱신
-        aboveLineStringEvent.RefreshString();
         storyLineStringEvent.RefreshString();
+
+        narkeImage.SetActive(isNarke);
+        echoImage.SetActive(!isNarke);
     }
 
     private void OnStoryLineReady(string localizedText)
     {
-        storyLineText.text = localizedText;
-        StartCoroutine(ShowNextButtonDelayed());
+        StopAllCoroutines();
+        StartCoroutine(TypeText(localizedText));
     }
 
-    private IEnumerator ShowNextButtonDelayed()
+    private IEnumerator TypeText(string fullText)
     {
+        storyLineText.text = "";
+        foreach (char c in fullText)
+        {
+            storyLineText.text += c;
+            yield return new WaitForSeconds(0.03f);
+        }
+
         yield return new WaitForSeconds(0.5f);
         nextButton.gameObject.SetActive(true);
     }
